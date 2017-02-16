27e3e2198a5b4c2a3a777e85fb1f4241a7eae9e5
Jack Ou <hi_jackou@hotmail.com>
2017-02-16 15:26:05 -0500

    Add files via upload

---
+++ Button.ios.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+import {
+ TouchableOpacity,
+  View,
+} from 'react-native';
+
+class Button extends Component{
+  constructor(props){
+      super(props);
+  }
+  render(){
+    return(
+      <TouchableOpacity {...this.props}>
+        {this.props.children}
+      </TouchableOpacity>
+    )
+  }
+}
+export default Button;
